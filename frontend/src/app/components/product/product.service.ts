ffcedac54acd22b35dc6b4ca68777c66f541c555
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-01 14:12:18 -0400

    added toast error for all methods

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -28,28 +28,40 @@ export class ProductService {
       catchError(e=>this.handleError(e))
     )
   }
-  
-  handleError(e:any):Observable<any>{
-    console.log(e)
-    this.showMessage("Ocorreu um erro!",true)
-    return EMPTY
-  }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl)
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map( (obj) => obj),
+      catchError(e=>this.handleError(e))
+    )
   }
 
   readById(id: number | null): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.get<Product>(url)
+    return this.http.get<Product>(url).pipe(
+      map( (obj) => obj),
+      catchError(e=>this.handleError(e))
+    )
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(
+      map( (obj) => obj),
+      catchError(e=>this.handleError(e))
+    )
   }
   delete(id: number | undefined): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url)
+    return this.http.delete<Product>(url).pipe(
+      map( (obj) => obj),
+      catchError(e=>this.handleError(e))
+    )
+  }
+
+  handleError(e:any):Observable<any>{
+    console.log(e)
+    this.showMessage("Ocorreu um erro!",true)
+    return EMPTY
   }
 }

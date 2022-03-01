8fad6d0d19dcad0b3a63e9844443bd3f24a0df80
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-01 13:11:56 -0400

    added update action on product

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  product: Product = {
+    name: '',
+    price: null
   }
 
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id ?? '').subscribe(product => {
+      this.product = product;
+    })
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!')
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
 }
